540fae4abaeb1e32ed84852fed005dec00af0280
Orestis <orestis.othonos@skillonnet.com>
2025-12-26 14:24:10 +0200

    Fix: 1.0.3

--- ZoiperConfigurator/Releases/1.0.3/Zoiper Configurator.ps1
+++ ZoiperConfigurator/Releases/1.0.3/Zoiper Configurator.ps1
@@ -1,7 +1,7 @@
 ﻿# Test
 # Zoiper 5 Setup Helper
 # This script prompts the user for Zoiper 5 credentials.
-$ScriptVersion = '1.0.2'
+$ScriptVersion = '1.0.3'
 
 # --- Self-update configuration ---
 # Set `UpdateUrl` to your public release download URL or leave empty and use the
